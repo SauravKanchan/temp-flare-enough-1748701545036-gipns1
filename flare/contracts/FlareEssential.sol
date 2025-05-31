1f6a59762aba2ebc8a5807a783714b44865abba8
Saurav  Kanchan <sauravnk30@gmail.com>
2025-05-31 16:26:11 +0200

    Copy flare/contracts/FlareEssential.sol from flare-enough

---
+++ flare/contracts/FlareEssential.sol
@@ -0,0 +1,5 @@
+import {IFdcHub} from "@flarenetwork/flare-periphery-contracts/coston2/IFdcHub.sol";
+import {IFdcRequestFeeConfigurations} from "@flarenetwork/flare-periphery-contracts/coston2/IFdcRequestFeeConfigurations.sol";
+import {IFlareSystemsManager} from "@flarenetwork/flare-periphery-contracts/coston2/IFlareSystemsManager.sol";
+import {IRelay} from "@flarenetwork/flare-periphery-contracts/coston2/IRelay.sol";
+import {IFlareContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/IFlareContractRegistry.sol";
